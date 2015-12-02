3d4685d129f772e9fec3c965d86d9b9c0e29a154
Alvaro Nistal <alvaro.nistal@gmail.com>
2015-12-02 11:01:10 +0100

    code improvments and correct log4j configuration

--- src/main/scala/com/stratio/election/runner/TwitterElectionRunner.scala
+++ src/main/scala/com/stratio/election/runner/TwitterElectionRunner.scala
@@ -18,6 +18,15 @@ object TwitterElectionRunner {
 
   private val logger = Logger.getLogger(this.getClass)
   private val KafkaTopic: String = "election"
+  private val userMention = ""
+  private val hashtag = ""
+  private val url = ""
+  private val media = ""
+  private val currentUserRetweetId = ""
+  private val user = ""
+  private val locationFirstLevel = ""
+  private val locationSecondLevel = ""
+  private val followersCount = 0
 
   implicit val formats: Formats = DefaultFormats
 
@@ -29,7 +38,7 @@ object TwitterElectionRunner {
     }
     def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
     def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
-    def onException(ex: Exception) { ex.printStackTrace }
+    def onException(ex: Exception) { logger.error(ex.getLocalizedMessage, ex) }
     def onScrubGeo(arg0: Long, arg1: Long) {}
     def onStallWarning(warning: StallWarning) {}
   }
@@ -55,6 +64,15 @@ object TwitterElectionRunner {
     twitterStream.filter(config.getStringList("trackWords").asScala.mkString(","))
   }
 
+  def processStatus(status: Status): Unit = {
+    logger.info(status.getText)
+    generateUnique(status)
+    generateHashtags(status)
+    generateUserMentions(status)
+    generateUrl(status)
+    generateMediaEntities(status)
+  }
+
   def getTwitterConfig(config: Config): Configuration = {
     val consumerKey = config.getString("consumerKey")
     val consumerSecret = config.getString("consumerSecret")
@@ -73,15 +91,6 @@ object TwitterElectionRunner {
     val id = status.getId
     val createdAt = status.getCreatedAt.getTime
     val text = status.getText
-    val userMention = ""
-    val hashtag = ""
-    val url = ""
-    val media = ""
-    val currentUserRetweetId = ""
-    val user = ""
-    val locationFirstLevel = ""
-    val locationSecondLevel = ""
-    val followersCount = 0
     val unique = "true"
 
     val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, url, media, currentUserRetweetId,
@@ -94,15 +103,6 @@ object TwitterElectionRunner {
     val id = status.getId
     val createdAt = status.getCreatedAt.getTime
     val text = status.getText
-    val userMention = ""
-    val hashtag = ""
-    val url = ""
-    val media = ""
-    val currentUserRetweetId = ""
-    val user = ""
-    val locationFirstLevel = ""
-    val locationSecondLevel = ""
-    val followersCount = 0
     val unique = "false"
 
     status.getHashtagEntities.map(hashtagEntity => {
@@ -117,15 +117,6 @@ object TwitterElectionRunner {
     val id = status.getId
     val createdAt = status.getCreatedAt.getTime
     val text = status.getText
-    val userMention = ""
-    val hashtag = ""
-    val url = ""
-    val media = ""
-    val currentUserRetweetId = ""
-    val user = ""
-    val locationFirstLevel = ""
-    val locationSecondLevel = ""
-    val followersCount = 0
     val unique = "false"
     status.getUserMentionEntities.map(userMentionEntity => {
       val twitterModel = TwitterModel(id, createdAt, text, userMentionEntity.getScreenName, hashtag, url, media,
@@ -139,15 +130,6 @@ object TwitterElectionRunner {
     val id = status.getId
     val createdAt = status.getCreatedAt.getTime
     val text = status.getText
-    val userMention = ""
-    val hashtag = ""
-    val url = ""
-    val media = ""
-    val currentUserRetweetId = ""
-    val user = ""
-    val locationFirstLevel = ""
-    val locationSecondLevel = ""
-    val followersCount = 0
     val unique = "false"
     status.getURLEntities.map(uRLEntity => {
       val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, uRLEntity.getExpandedURL, media,
@@ -161,15 +143,6 @@ object TwitterElectionRunner {
     val id = status.getId
     val createdAt = status.getCreatedAt.getTime
     val text = status.getText
-    val userMention = ""
-    val hashtag = ""
-    val url = ""
-    val media = ""
-    val currentUserRetweetId = ""
-    val user = ""
-    val locationFirstLevel = ""
-    val locationSecondLevel = ""
-    val followersCount = 0
     val unique = "false"
     status.getMediaEntities.map(mediaEntity => {
       val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, url, mediaEntity.getExpandedURL,
@@ -177,14 +150,5 @@ object TwitterElectionRunner {
         user, locationFirstLevel, locationSecondLevel, followersCount, unique)
       KafkaProducer.put(producer, KafkaTopic ,write(twitterModel))
     })
-  }
-
-  def processStatus(status: Status): Unit = {
-    println(status.getText)
-    generateUnique(status)
-    generateHashtags(status)
-    generateUserMentions(status)
-    generateUrl(status)
-    generateMediaEntities(status)
   }
 }
