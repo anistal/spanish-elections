5c415eb7f920b57b5765c134d3128c21951af30c
Alvaro Nistal <alvaro.nistal@gmail.com>
2015-12-09 18:38:57 +0100

    all streams working

--- src/main/scala/com/stratio/election/model/TwitterModel.scala
+++ src/main/scala/com/stratio/election/model/TwitterModel.scala
@@ -1,17 +1,18 @@
 package com.stratio.election.model
 
-case class TwitterModel(idTweet: Long,
-                        createdAt: Long,
-                        text: String,
-                        userMention: String,
-                        hashtag: String,
-                        url: String,
-                        media: String,
-                        currentUserRetweetId: Long,
-                        user: String,
-                        locationFirstLevel: String,
-                        locationSecondLevel: String,
-                        followersCount: Int,
-                        retweetCount: Int,
-                        favorited: String,
-                        unique: String) {}
+import java.util.Date
+
+case class TwitterModel(idTweet: Long = 0L,
+                        createdAt: Long = new Date().getTime,
+                        text: String = "No text",
+                        userMention: String = "No mention",
+                        hashtag: String = "No hashtag",
+                        url: String = "No url",
+                        media: String = "No media",
+                        currentUserRetweetId: Long = 0L,
+                        user: String = "No user",
+                        userId: Long = 0L,
+                        location: String = "No location",
+                        followersCount: Int = 0,
+                        retweetId: Long = 0L,
+                        retweetCount: Int = 0) {}
--- src/main/scala/com/stratio/election/runner/TwitterElectionRunner.scala
+++ src/main/scala/com/stratio/election/runner/TwitterElectionRunner.scala
@@ -12,44 +12,58 @@ import org.json4s.native.Serialization._
 import org.json4s.{DefaultFormats, Formats}
 
 import scala.collection.JavaConverters._
+import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 object TwitterElectionRunner {
 
   private val logger = Logger.getLogger(this.getClass)
-  private val KafkaTopic: String = "election"
-  private val userMention = ""
-  private val hashtag = ""
-  private val url = ""
-  private val media = ""
-
+  private val Empty = ""
+  private val MinRetweetCount = 10
   implicit val formats: Formats = DefaultFormats
 
-  var producer: Producer[String,String] = _
+  var areas: Map[String, String] = Map()
+
+  var producer: Producer[String, String] = _
 
   private val twitterListener = new StatusListener() {
     def onStatus(status: Status) {
       processStatus(status)
     }
+
     def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+
     def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
-    def onException(ex: Exception) { logger.error(ex.getLocalizedMessage, ex) }
+
+    def onException(ex: Exception) {
+      logger.error(ex.getLocalizedMessage, ex)
+    }
+
     def onScrubGeo(arg0: Long, arg1: Long) {}
+
     def onStallWarning(warning: StallWarning) {}
   }
 
   def main(args: Array[String]) = {
-    if(args.size == 0) {
+    if (args.size == 0) {
       logger.info("Use: java -jar twitter-election.jar <config file>")
       System.exit(1)
     }
-   Try(ConfigFactory.parseFile(new File(args(0)))) match {
-      case Success(config) => start(config)
+    Try(ConfigFactory.parseFile(new File(args(0)))) match {
+      case Success(config) =>
+        initializeAreas()
+        start(config)
       case Failure(exception) => {
         logger.error(exception.getLocalizedMessage, exception)
         System.exit(1)
       }
     }
+  }
+
+  def initializeAreas(): Unit = {
+    val areasLines = Source.fromInputStream(this.getClass.getClassLoader
+      .getResourceAsStream("areas.txt")).getLines().toSeq
+    areas = areasLines.map(line => line.split(",")(0) -> line.split(",")(1)).toMap
   }
 
   def start(config: Config): Unit = {
@@ -61,11 +75,76 @@ object TwitterElectionRunner {
 
   def processStatus(status: Status): Unit = {
     logger.info(s">> ${status.getText}")
-    generateUnique(status)
-    generateHashtags(status)
-    generateUserMentions(status)
-    generateUrl(status)
-    generateMediaEntities(status)
+    val event = generateEvent(status)
+    generateHashtags(status, event)
+    generateUserMentions(status, event)
+    generateUrl(status, event)
+    generateMediaEntities(status, event)
+    generateRetweets(status, event)
+  }
+
+  // XXX
+
+  def generateRetweets(status: Status, event: TwitterModel) = {
+    if (event.retweetCount >= MinRetweetCount && event.retweetId != 0)
+      KafkaProducer.put(producer, "retweets", write(event))
+  }
+
+  def generateHashtags(status: Status, event: TwitterModel) = {
+    status.getHashtagEntities.map(hashtagEntity => {
+      val twitterModel = event.copy(hashtag = hashtagEntity.getText)
+      KafkaProducer.put(producer, "hashtags", write(twitterModel))
+      KafkaProducer.put(producer, "influenceHashtags", write(twitterModel))
+    })
+  }
+
+  def generateUserMentions(status: Status, event: TwitterModel) = {
+    status.getUserMentionEntities.map(userMentionEntity => {
+      val twitterModel = event.copy(userMention = userMentionEntity.getScreenName)
+      KafkaProducer.put(producer, "usermentions", write(twitterModel))
+      KafkaProducer.put(producer, "influenceMentions", write(twitterModel))
+    })
+  }
+
+  def generateUrl(status: Status, event: TwitterModel) = {
+    status.getURLEntities.map(uRLEntity => {
+      val twitterModel = event.copy(url = uRLEntity.getURL)
+      KafkaProducer.put(producer, "urls", write(twitterModel))
+    })
+  }
+
+  def generateMediaEntities(status: Status, event: TwitterModel) = {
+    status.getMediaEntities.map(mediaEntity => {
+      val twitterModel = event.copy(media = mediaEntity.getMediaURL)
+      KafkaProducer.put(producer, "mediaentities", write(twitterModel))
+    })
+  }
+
+  // XXX
+
+  def generateEvent(status: Status): TwitterModel = {
+    val id = status.getId
+    val createdAt = status.getCreatedAt.getTime
+    val text = status.getText
+
+
+
+    val currentUserRetweetId = status.getCurrentUserRetweetId
+    val user = if (status.getCurrentUserRetweetId == -1) status.getUser.getScreenName else ""
+    val userId = if (status.getCurrentUserRetweetId == -1) status.getUser.getId else status.getCurrentUserRetweetId
+
+    val location = if (status.getUser.getLocation != null) {
+      val filterAreas = areas.filter(area => status.getUser.getLocation.toLowerCase.contains(area._1))
+      if (filterAreas.size != 0) filterAreas.head._2 else "No location"
+    } else "No location"
+
+    val followersCount = status.getUser.getFollowersCount
+
+    val retweetId = if(status.getRetweetedStatus != null) status.getRetweetedStatus.getId else 0
+    val retweetCount = if(status.getRetweetedStatus != null) status.getRetweetedStatus.getRetweetCount else 0
+
+    TwitterModel(id, createdAt, text, Empty, Empty, Empty, Empty, currentUserRetweetId, user, userId, location,
+      followersCount, retweetId, retweetCount)
   }
 
   def getTwitterConfig(config: Config): Configuration = {
@@ -80,165 +159,5 @@ object TwitterElectionRunner {
       .setOAuthAccessToken(accessToken)
       .setOAuthAccessTokenSecret(accessTokenSecret)
       .build
-  }
-
-  def generateUnique(status: Status) = {
-    val id = status.getId
-    val createdAt = status.getCreatedAt.getTime
-    val text = status.getText
-
-    val currentUserRetweetId = status.getCurrentUserRetweetId
-    val user = status.getUser.getScreenName
-    val locationFirstLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(0).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val locationSecondLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(1).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val followersCount = status.getUser.getFollowersCount
-    val retweetCount = status.getRetweetCount
-    val favorited = status.isFavorited.toString
-
-    val unique = "true"
-
-    val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, url, media, currentUserRetweetId,
-      user, locationFirstLevel, locationSecondLevel, followersCount, retweetCount, favorited, unique)
-
-    KafkaProducer.put(producer, KafkaTopic ,write(twitterModel))
-  }
-
-  def generateHashtags(status: Status) = {
-    val id = status.getId
-    val createdAt = status.getCreatedAt.getTime
-    val text = status.getText
-
-    val currentUserRetweetId = status.getCurrentUserRetweetId
-    val user = status.getUser.getScreenName
-    val locationFirstLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(0).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val locationSecondLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(1).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val followersCount = status.getUser.getFollowersCount
-    val retweetCount = status.getRetweetCount
-    val favorited = status.isFavorited.toString
-
-    val unique = "false"
-
-    status.getHashtagEntities.map(hashtagEntity => {
-      val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtagEntity.getText, url, media,
-        currentUserRetweetId, user, locationFirstLevel, locationSecondLevel, followersCount, retweetCount,
-        favorited, unique)
-      KafkaProducer.put(producer, KafkaTopic ,write(twitterModel))
-    })
-  }
-
-  def generateUserMentions(status: Status) = {
-    val id = status.getId
-    val createdAt = status.getCreatedAt.getTime
-    val text = status.getText
-
-    val currentUserRetweetId = status.getCurrentUserRetweetId
-    val user = status.getUser.getScreenName
-    val locationFirstLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(0).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val locationSecondLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(1).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val followersCount = status.getUser.getFollowersCount
-    val retweetCount = status.getRetweetCount
-    val favorited = status.isFavorited.toString
-
-    val unique = "false"
-    status.getUserMentionEntities.map(userMentionEntity => {
-      val twitterModel = TwitterModel(id, createdAt, text, userMentionEntity.getScreenName, hashtag, url, media,
-        currentUserRetweetId,
-        user, locationFirstLevel, locationSecondLevel, followersCount, retweetCount, favorited, unique)
-      KafkaProducer.put(producer, KafkaTopic ,write(twitterModel))
-    })
-  }
-
-  def generateUrl(status: Status) = {
-    val id = status.getId
-    val createdAt = status.getCreatedAt.getTime
-    val text = status.getText
-
-    val currentUserRetweetId = status.getCurrentUserRetweetId
-    val user = status.getUser.getScreenName
-    val locationFirstLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(0).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val locationSecondLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(1).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val followersCount = status.getUser.getFollowersCount
-    val retweetCount = status.getRetweetCount
-    val favorited = status.isFavorited.toString
-
-    val unique = "false"
-    status.getURLEntities.map(uRLEntity => {
-      val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, uRLEntity.getExpandedURL, media,
-        currentUserRetweetId,
-        user, locationFirstLevel, locationSecondLevel, followersCount, retweetCount, favorited, unique)
-      KafkaProducer.put(producer, KafkaTopic , write(twitterModel))
-    })
-  }
-
-  def generateMediaEntities(status: Status) = {
-    val id = status.getId
-    val createdAt = status.getCreatedAt.getTime
-    val text = status.getText
-
-    val currentUserRetweetId = status.getCurrentUserRetweetId
-    val user = status.getUser.getScreenName
-    val locationFirstLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(0).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val locationSecondLevel = if(status.getUser.getLocation != null) {
-      Try(status.getUser.getLocation.split(",")(1).trim) match {
-        case Success(value) => value
-        case Failure(exception) => ""
-      }
-    }  else ""
-    val followersCount = status.getUser.getFollowersCount
-    val retweetCount = status.getRetweetCount
-    val favorited = status.isFavorited.toString
-
-    val unique = "false"
-    status.getMediaEntities.map(mediaEntity => {
-      val twitterModel = TwitterModel(id, createdAt, text, userMention, hashtag, url, mediaEntity.getURL,
-        currentUserRetweetId, user, locationFirstLevel, locationSecondLevel, followersCount, retweetCount, favorited,
-          unique)
-      KafkaProducer.put(producer, KafkaTopic, write(twitterModel))
-    })
   }
 }
